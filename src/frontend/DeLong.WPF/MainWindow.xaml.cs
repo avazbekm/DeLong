ab9b16626cbcce4a686d85de75f52abde4d4847c
misterpozitiv1607 <mansurjonmoydinov16072002@gmail.com>
2024-12-04 09:24:44 +0500

    Resource Faylni qo'shish

--- src/frontend/DeLong.WPF/MainWindow.xaml.cs
+++ src/frontend/DeLong.WPF/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace DeLong.WPF;
 
@@ -10,5 +12,27 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private string _currentLanguage = "en"; // Tanlangan tilni saqlash uchun o'zgaruvchi
+
+    private void LanguageAPP(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+    {
+        if (languageComboBox.SelectedItem is ComboBoxItem selectedItem)
+        {
+            string selectedLanguage = selectedItem.Tag.ToString();
+
+            if (_currentLanguage != selectedLanguage) // Faqat til o'zgarishida yangilash
+            {
+                _currentLanguage = selectedLanguage;
+                DeLong.WPF.Resources.Resource.Culture = new CultureInfo(selectedLanguage);
+                UpdateLanguage(); // Matnlarni yangilash
+            }
+        }
+    }
+    private void UpdateLanguage()
+    {
+        
+        
     }
 }
