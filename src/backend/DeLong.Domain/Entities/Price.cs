ee1426e7ca36332d4f5e33a2a59d49e45158ed19
Avazbek0704 <siddikovavazbek213@gmail.com>
2025-01-02 11:29:30 +0500

    edited price entity

--- src/backend/DeLong.Domain/Entities/Price.cs
+++ src/backend/DeLong.Domain/Entities/Price.cs
@@ -8,6 +8,7 @@ public class Price : Auditable
     public decimal SellingPrice { get; set; }  // Sotish narxi
     public string UnitOfMeasure { get; set; } = string.Empty; // kg,dona,karobka,litr
     public int Quantity { get; set; } // miqdori
+
     public long ProductId { get; set; }
     public Product Product { get; set; }
 }
