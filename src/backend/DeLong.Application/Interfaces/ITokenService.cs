5456627dfa42d4d01d436d722d52d6fd3788073d
Mansurjon Moydinov <mansurjonmoydinov16072002@gmail.com>
2025-03-20 12:08:52 +0500

    JWT and Refresh token added

---
+++ src/backend/DeLong.Application/Interfaces/ITokenService.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Claims;
+
+namespace DeLong.Service.Interfaces;
+
+public interface ITokenService
+{
+    string GenerateAccessToken(IEnumerable<Claim> claims);
+    string GenerateRefreshToken();
+}
