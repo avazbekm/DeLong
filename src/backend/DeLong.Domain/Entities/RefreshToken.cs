79b2f00403403422d40c6a29ab4a15b44b7036c6
Mansurjon Moydinov <mansurjonmoydinov16072002@gmail.com>
2025-03-20 12:11:08 +0500

    Refresh token entity created

---
+++ src/backend/DeLong.Domain/Entities/RefreshToken.cs
@@ -0,0 +1,9 @@
+﻿namespace DeLong.Domain.Entities;
+
+public class RefreshToken
+{
+    public int Id { get; set; }
+    public string Username { get; set; } = null!;
+    public string Token { get; set; } = null!;
+    public DateTime ExpiryDate { get; set; }
+}
