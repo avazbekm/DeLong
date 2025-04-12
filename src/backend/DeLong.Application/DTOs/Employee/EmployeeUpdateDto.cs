4206842b32a66444d08888bdc7994dec718bce4e
Avazbek0704 <siddikovavazbek213@gmail.com>
2025-04-12 10:08:47 +0500

    changed Customer and Employee entities

--- src/backend/DeLong.Application/DTOs/Employee/EmployeeUpdateDto.cs
+++ src/backend/DeLong.Application/DTOs/Employee/EmployeeUpdateDto.cs
@@ -1,11 +1,16 @@
 ﻿
+using DeLong.Domain.Enums;
+
 namespace DeLong.Service.DTOs.Employee;
 
 public class EmployeeUpdateDto
 {
     public long Id { get; set; }
-    public long UserId { get; set; }
-    public long BranchId { get; set; } // hodim qaysi ombor bilan ishlashini aniqlash uchun
+    public string FullName { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+    public Role Role { get; set; }
     public string Username { get; set; } = string.Empty;  // Foydalanuvchi nomi (Login)
     public string Password { get; set; } = string.Empty; // Parol (hashlangan)
+
+    public long BranchId { get; set; } // hodim qaysi filial bilan ishlashini aniqlash uchun
 }
--- src/backend/DeLong.Domain/Entities/Customer.cs
+++ src/backend/DeLong.Domain/Entities/Customer.cs
@@ -4,18 +4,16 @@ namespace DeLong.Domain.Entities;
 
 public class Customer : Auditable
 {
-    public string Name { get; set; } = string.Empty;
+    public string CompanyName { get; set; } = string.Empty;
+    public string ManagerName {  get; set; } = string.Empty;
+    public string ManagerPhone { get; set; } = string.Empty;
+    public string? MFO { get; set; } = string.Empty;
     public int? INN { get; set; }
-    public string? JSHSHIR { get; set; }
-    public string Phone { get; set; } = string.Empty;
-    public string MFO { get; set; } = string.Empty;
-    public string BankAccount { get; set; } = string.Empty;
-    public string BankName { get; set; } = string.Empty;
+    public string? BankAccount { get; set; } = string.Empty;
+    public string? BankName { get; set; } = string.Empty;
     public string? OKONX { get; set; }
-    public string YurAddress { get; set; } = string.Empty;
-
-    // firma rahbari malumotlari
-    public long UserId { get; set; }
-    public required User User { get; set; }
+    public string? YurAddress { get; set; } = string.Empty;
+    public string? EmployeeName { get; set; } = string.Empty;
+    public string? EmployeePhone { get; set; } = string.Empty;
     public long BranchId { get; set; }
 }
